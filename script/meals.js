2bff4c15e71dc2238899b9cd475cc2df1c6cf66b
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-09-02 21:04:18 +0600

    ds

--- script/meals.js
+++ script/meals.js
@@ -18,6 +18,7 @@ const displayMeals = (meals) => {
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
                     <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+                    <button class="d-block mt-5 mx-auto btn btn-primary" onclick="loadMealDetail(${meal.idMeal})">View Recipe</button>
                 </div>
             </div>
         `;
@@ -32,4 +33,34 @@ const searchFood = () => {
     loadMeals(searchText);
     searchField.value = '';
     loadMeals(searchText);
+}
+
+const loadMealDetail = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = (meal) => {
+  const detailContainer = document.getElementById('detail-container');
+  detailContainer.innerHTML='';
+  const mealDiv = document.createElement('div');
+  mealDiv.classList.add('card');
+  mealDiv.classList.add('mb-3');
+  mealDiv.innerHTML = `
+  <div class="row g-0">
+      <div class="col-md-4">
+          <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+          <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions}</p>
+          </div>
+      </div>
+  </div>
+  `;
+  detailContainer.appendChild(mealDiv);
 }
